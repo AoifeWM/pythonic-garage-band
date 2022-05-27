172e2281c99ec81e9451dfb1e65033003f3017fd
AoifeWM <aoifewmulligan@gmail.com>
2022-05-26 21:29:04 -0700

    added readme

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,7 +1,10 @@
 class Band:
+    instances = []
+
     def __init__(self, name, members=None):
         self.name = name
         self.members = members
+        self.instances.append(self)
 
     def __str__(self):
         return f"The band {self.name}"
@@ -9,18 +12,48 @@ class Band:
     def __repr__(self):
         return f"Band instance. name={self.name}, members={self.members}"
 
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.solo)
+        return solos
+
+    @classmethod
+    def to_list(cls):
+        print(cls.instances)
+        return cls.instances
+
 
 class Musician:
-    pass
+    def __init__ (self, name, instrument="bad subclass", solo="bad subclass"):
+        self.name = name
+        self.instrument = instrument
+        self.solo = solo
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"{type(self).__name__} instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return self.instrument
+
+    def play_solo(self):
+        return self.solo
 
 
-class Guitarist:
-    pass
+class Guitarist(Musician):
+    def __init__(self, name, instrument="guitar", solo="face melting guitar solo"):
+        Musician.__init__(self, name, instrument, solo)
 
 
-class Bassist:
-    pass
+class Bassist(Musician):
+    def __init__(self, name, instrument="bass", solo="bom bom buh bom"):
+        Musician.__init__(self, name, instrument, solo)
 
 
-class Drummer:
-    pass
+class Drummer(Musician):
+    def __init__(self, name, instrument="drums", solo="rattle boom crash"):
+        Musician.__init__(self, name, instrument, solo)
+
